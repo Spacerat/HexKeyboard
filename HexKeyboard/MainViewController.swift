54f0935b05d0d864551f9604fa672aa8bee8a6f2
Joseph Atkins-Turkish <jat1g11@soton.ac.uk>
2014-12-13 13:46:24 +0000

    Added MainViewController for the main view controller.

---
+++ HexKeyboard/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HexKeyboard
+//
+//  Created by Joseph Atkins-Turkish on 02/09/2014.
+//  Copyright (c) 2014 Joseph Atkins-Turkish. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        let col = UIColor(red: 255, green: 100, blue: 100, alpha: 1)
+//        clearButton.backgroundColor = col
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
